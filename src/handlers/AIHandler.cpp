1c82be4ed2877d73cda45ea5df0f76f4fa6d7513
kokol16 <kokol1616@gmail.com>
2022-02-09 01:00:54 +0200

    me agapi

--- src/handlers/AIHandler.cpp
+++ src/handlers/AIHandler.cpp
@@ -107,4 +107,31 @@ void Game::AIHandler(void) {
 		//s->SetFrame((s->GetFrame() + 1) % s->currFilm->GetTotalFrames());
 	}
 
+	for (Sprite* s : SpriteManager::GetSingleton().GetTypeList("boomerang")) {
+		if (s->bDead) {
+			continue;
+
+
+		}
+
+		if (s->bLooking) {
+			if (this->mMap->GetTileLayer()->GetGridLayer()->CanMoveRight(s->GetBox())) {
+				s->CallAction("run.right");
+
+			}
+			else {
+				s->CallAction("run.left");
+			}
+		}
+		else {
+			if (this->mMap->GetTileLayer()->GetGridLayer()->CanMoveLeft(s->GetBox())) {
+				s->CallAction("run.left");
+			}
+			else {
+				s->CallAction("run.right");
+			}
+		}
+		//s->SetFrame((s->GetFrame() + 1) % s->currFilm->GetTotalFrames());
+	}
+
 }
--- src/handlers/InputHandler.cpp
+++ src/handlers/InputHandler.cpp
@@ -11,7 +11,6 @@ void Game::InputHandler(void) {
 	ALLEGRO_EVENT events;
 	al_wait_for_event(event_queue, &events);
 	al_get_keyboard_state(&ksKeyboardState);
-
 	Sprite* sMario = nullptr;
 	for (auto obj : SpriteManager::GetSingleton().GetTypeList("main")) {
 		sMario = obj;
@@ -29,7 +28,7 @@ void Game::InputHandler(void) {
 		if (bIsGameEnding) {
 
 			sMario->CallAction("run.right");
-			
+
 			if (sMario->x == 296 * 16 && sMario->y == 88 * 16) {	//flag
 				bIsGameEnding = false;
 				sMario->bLooking = false;
@@ -41,7 +40,8 @@ void Game::InputHandler(void) {
 				sMario->CallAction("idle");
 			}
 
-		}else if (bInputAllowed) {
+		}
+		else if (bInputAllowed) {
 
 			if (al_key_down(&ksKeyboardState, ALLEGRO_KEY_Z)) {
 				sMario->dx = 3;
@@ -87,6 +87,74 @@ void Game::InputHandler(void) {
 
 					//Audio::singleton.playSample("config/sounds/sword-hit.mp3", ALLEGRO_PLAYMODE_ONCE);
 					sMario->CallAction("attack.sword");
+					bDidSomething = true;
+
+				}
+			}
+
+
+			if (al_key_down(&ksKeyboardState, ALLEGRO_KEY_C)) {
+				if (sMario->id == "herochar") {
+
+					//Audio::singleton.playSample("config/sounds/sword-hit.mp3", ALLEGRO_PLAYMODE_ONCE);
+					//sMario->CallAction("attack.sword");
+					Sprite* boomerang = SpriteManager::GetSingleton().SpawnSprite(Config::GetConfig("config/sprites/boomerang.json"), "boomerang", "boomerang", sMario->x + 4, sMario->y, this->mMap->GetTileLayer()->GetGridLayer(), this);
+
+					boomerang->bDead = false;
+
+					//boomerang->GetGravityHandler().Disable();
+					boomerang->RegisterDefaultActions();
+
+
+					for (Sprite* obj : SpriteManager::GetSingleton().GetDisplayList()) {
+						if (obj->id.find("boomerang") != std::string::npos) {
+							continue;
+						}
+						if (obj == boomerang || obj == sMario) continue;
+						CollisionChecker::GetSingleton().Register(obj, boomerang,
+							[](Sprite* s1, Sprite* s2) {
+
+								s2->bDead = true;
+								SpriteManager::GetSingleton().Remove(s2);
+								CollisionChecker::GetSingleton().Cancel(s1, s2);
+
+
+
+								if (s1->id.find("turtle") != std::string::npos || s1->id.find("goomba") != std::string::npos || s1->id.find("slime") != std::string::npos) {
+									std::cout << "collision damaged id: " << s1->id << std::endl;
+									s1->CallAction("damage");
+								}
+
+
+
+
+
+
+							}
+						);
+					}
+
+					/*Animator* pAnim, * pAnimBoomer = NULL;
+
+
+					pAnimBoomer = AnimatorManager::GetSingleton().GetAnimatorByAnimationID("boomerang.run.right");
+
+
+					if (pAnimBoomer && pAnimBoomer->HasFinished()) {
+
+						Sprite * boomerang=SpriteManager::GetSingleton().SpawnSprite(Config::GetConfig("config/sprites/boomerang.json"), "boomerang", "boomerang", sMario->x + 4, sMario->y, this->mMap->GetTileLayer()->GetGridLayer(), this);
+
+						//sMario->currFilm = FilmHolder::Get().GetFilm("mario.walking.right");
+						if (sMario->bLooking)
+							boomerang->currFilm = FilmHolder::Get().GetFilm("boomerang.run.right");
+						//else
+							//s->currFilm = FilmHolder::Get().GetFilm(s->GetTrimmedID() + ".attack.sword.left");
+						((FrameListAnimator*)pAnimBoomer)->Start(((FrameListAnimator*)pAnimBoomer)->getAnimation(), SystemClock::Get().getgametime());
+						AnimatorManager::GetSingleton().MarkAsRunning(pAnimBoomer);
+					}
+					*/
+
+
 					bDidSomething = true;
 
 				}
